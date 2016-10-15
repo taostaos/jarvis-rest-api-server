5c8154b3dec31cb956815d3bc4b887a9f4a2c34b
taostaos <thomas.brizard@gmail.com>
2016-10-15 16:35:15 +0200

    First implementation

---
+++ README.md
@@ -0,0 +1,11 @@
+# jarvis-rest-api-server
+
+Simple Flask rest api server to make jarvis say something.
+
+    python jarvis-rest-api-server.py
+
+You can edit the config inside properties.txt: jarvis_home, host, port
+
+Example api call : 
+
+    curl -X POST -H "Content-Type: application/json" -d '{"message":"test pour jarvis"}' http://127.0.0.1:5000/jarvissay
---
+++ jarvis_server_api.py
@@ -0,0 +1,19 @@
+from flask import Flask,json,request
+import subprocess
+app = Flask(__name__)
+
+props = dict(line.strip().split('=') for line in open('properties.txt'))
+
+
+@app.route("/jarvissay", methods=['POST'])
+def jarvis_say():
+    if request.json:
+      command="bash "+jarvis_home+"jarvis.sh -s \'"+request.json.get("message")+"\'"
+      subprocess.call(command, shell=True)
+    else:
+        return "415 Unsupported Media Type"
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host=props("ip_host"),port=props("port_flask"))
+
