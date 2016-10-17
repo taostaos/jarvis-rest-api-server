4d283c5d612113ce9bf2991b018e6506353838d4
taostaos <thomas.brizard@gmail.com>
2016-10-17 19:31:30 +0200

    feat:changed properties.txt to properties

--- jarvis_server_api.py
+++ jarvis_server_api.py
@@ -1,8 +1,13 @@
+# Simple Flask rest api server to make jarvis say something.
+# You can edit the config inside properties.txt: jarvis_home, host, port
+# Example api call : 
+# curl -X POST -H "Content-Type: application/json" -d '{"message":"test pour jarvis"}' http://127.0.0.1:6543/jarvissay
+
 from flask import Flask,json,request
 import subprocess
 app = Flask(__name__)
 
-props = dict(line.strip().split('=') for line in open('properties.txt'))
+props = dict(line.strip().split('=') for line in open('properties'))
 
 ip_host=props["ip_host"]
 port_flask=int(props["port_flask"])
